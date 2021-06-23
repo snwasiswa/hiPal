18b420080b8eec7f02b05a3e262247a79b1bffa3
snwasiswa <stevewasiswa@gmail.com>
2021-06-23 15:54:40 -0500

    New pages and updates

--- hipal/languages/urls.py
+++ hipal/languages/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from languages import views
+from .import views
 
 # Customization of django admin
 admin.site.site_header = "hiPal"
 admin.site.site_title = "Welcome to hiPal's Dashboard"
 admin.site.index_title = "Welcome to the Portal"
 
+# Patterns of different paths
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('mine/', views.ManageLessonView.as_view(), name='manage_lesson_list'),
+    path('create', views.LessonCreateView.as_view(), name='lesson_create'),
+    path('<pk>/edit/', views.LessonUpdateView.as_view(), name='lesson_edit'),
+    path('<pk>/delete/', views.LessonDeleteView.as_view(), name='lesson_delete'),
+
 ]
--- hipal/languages/views.py
+++ hipal/languages/views.py
@@ -3,25 +3,26 @@ from .models import Language, Lesson, Module, Content
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 # Create your views here.
 class LessonListView(ListView):
     """View for the list of lessons"""
     model = Lesson
-    template_name = 'languages/manage/lesson/list.html'
+    template_name = 'languages/management/lesson/list_lesson.html'
 
     def get_queryset(self):
         """Allows to only display or update the lesson created"""
         queryset = super().get_queryset()
-        return queryset.filter(creator = self.request.user)
+        return queryset.filter(creator=self.request.user)
 
 
 class CreatorMixin(object):
     def get_queryset(self):
         """Allows to only display or update the lesson created"""
         queryset = super().get_queryset()
-        return queryset.filter(creator = self.request.user)
+        return queryset.filter(creator=self.request.user)
 
 
 class EditableCreatorMixin(object):
@@ -30,30 +31,32 @@ class EditableCreatorMixin(object):
         return super().validate(form)
 
 
-class CreatorLessonMixin(CreatorMixin):
+class CreatorLessonMixin(CreatorMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Lesson
-    fields = ['language', 'title','slug','overview','created_on']
+    fields = ['language', 'title', 'slug', 'overview', 'created_on']
     success_url = reverse_lazy('manage_lesson')
 
 
 class EditableCreatorMixinLesson(CreatorLessonMixin, EditableCreatorMixin):
-    template_name = 'languages/manage/lesson/list.html'
+    template_name = 'languages/management/lesson/list_lesson.html'
 
 
 class ManageLessonView(CreatorLessonMixin, ListView):
-    template_name = 'languages/manage/lesson/list'
+    template_name = 'languages/management/lesson/list_lesson.html'
+    permission_required = 'languages.view_lesson'
 
 
 class LessonCreateView(EditableCreatorMixinLesson, CreateView):
-    pass
+    permission_required = 'languages.add_lesson'
 
 
 class LessonUpdateView(EditableCreatorMixinLesson, UpdateView):
-    pass
+    permission_required = 'languages.change_lesson'
 
 
 class LessonDeleteView(CreatorLessonMixin, DeleteView):
-    template_name = 'languages/manage/lesson/delete_lesson.list'
+    template_name = 'languages/management/lesson/delete_lesson.list'
+    permission_required = 'languages.delete_lesson'
 
 
 def homepage(request):
