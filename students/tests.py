de77b83c7f3c28b6c7a1b088e4642a32c9651124
Steve Wasiswa <steve-wasiswa@uiowa.edu>
2021-09-05 11:45:17 -0500

    fixing urls

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
